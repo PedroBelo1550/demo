41028ba80e70a0d100b7f73bd32e890b92b16088
pedro.belo <pedrohenrique1550@hotmail.com>
2023-03-09 23:56:23 -0300

    cxv

--- src/main/java/br/example/demo/service/interfaces/ClienteRepostory.java
+++ src/main/java/br/example/demo/service/interfaces/ClienteRepostory.java
@@ -2,6 +2,9 @@ package br.example.demo.service.interfaces;
 
 import java.util.List;
 
+/**
+ * teste
+ */
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.example.demo.service.model.Cliente;
--- src/main/java/br/example/demo/service/interfaces/ComprasRepository.java
+++ src/main/java/br/example/demo/service/interfaces/ComprasRepository.java
@@ -1,7 +1,11 @@
 package br.example.demo.service.interfaces;
 
 import java.util.List;
+/* 
 
+ckaokfsofjsdojovjriovjeriofjeriojvreiovmreoivjeroivjreio
+
+*/ 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.example.demo.service.model.Compras;
